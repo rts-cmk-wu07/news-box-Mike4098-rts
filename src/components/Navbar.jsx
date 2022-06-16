3cc95adecb8ca51b2a12a15ef5546dbe431f4c61
Mike <U10604@rts-365.dk>
2022-06-16 14:23:07 +0200

    added css variables. Getting ready for designing.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,36 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import {Link} from "react-router-dom"
 const Navbar = () => {
+    const style = {
+        navDiv: css`
+        width: 100%;
+        margin: 0px;
+       
+        `,
+        nav:css`
+         display: flex;
+       align-items:center;
+       justify-content: space-evenly;
+       position: fixed;
+       top: 0;
+        background-color: #ffffff;
+        width: 100%;
+        height: 2em;
+        margin: 0px;
+        padding: 0;
+        border-bottom: 1px solid #1b1c205f;
+        `,
+        h1:css`
+        font-size: 30px;
+        font-weight: bold;
+        `
+    }
     return ( 
-        <div className="navDiv">
-            <nav className="navbar">
+        <div className="navDiv" css={style.navDiv}>
+            <nav className="navbar" css={style.nav}>
                 <Link to="/">Home</Link>
+                <h1 css={style.h1}>News box</h1>
             <Link to="/Settings">Settings</Link>
             </nav>
         </div>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Settings from './components/Settings/Settings';
-
+import './index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
